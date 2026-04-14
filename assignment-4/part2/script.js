57dbf32cd155612a2123fa95347153cfc9579510
javierfalcon1 <javierfalcon08@gmail.com>
2026-04-13 21:58:23 -0400

    commit 4
    darken and lighten button

--- assignment-4/part2/script.js
+++ assignment-4/part2/script.js
@@ -19,3 +19,31 @@ const images = [
   { filename: "pic5.jpg", alt: "Large moth on a leaf" },
 
 ];
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener("click", updateDisplayedImage);
+}
+
+function updateDisplayedImage(e) {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+}
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  }else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+  btn.classList.toggle("dark");
+});
