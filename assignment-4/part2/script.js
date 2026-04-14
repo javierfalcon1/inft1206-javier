f6c3269d7d3c62133e355e3af3caf7087e02cbe9
javierfalcon1 <javierfalcon08@gmail.com>
2026-04-13 21:33:28 -0400

    commit 2
    HTML5 validation, added top comment to all files,  added javascript to html file by using the script tag

--- assignment-4/part2/script.js
+++ assignment-4/part2/script.js
@@ -1,3 +1,10 @@
+/* 
+Name: Javier Falcon 
+File: index.html 
+Date: 13 April 2026 
+Description: the html file for an image gallery  
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
