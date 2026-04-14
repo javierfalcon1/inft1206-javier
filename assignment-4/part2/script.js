d47693c41679c5700f3decb63c98e7ef2313323c
javierfalcon1 <javierfalcon08@gmail.com>
2026-04-13 22:15:18 -0400

    commit 5 final
    added css

--- assignment-4/part2/script.js
+++ assignment-4/part2/script.js
@@ -7,9 +7,10 @@ Description: the html file for an image gallery
 
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
-
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+btn.classList.remove("dark");
 
 const images = [
   { filename: "pic1.jpg", alt: "Closeup of a human eye" },
