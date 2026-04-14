a50c9673358ad4ba8a46c0edd87cf927e1b11081
javierfalcon1 <javierfalcon08@gmail.com>
2026-04-13 20:24:58 -0400

    commit 4
    put characters, places, and events in separate arrays. completed the returnRandomStoryString function.

--- assignment-4/part1/script.js
+++ assignment-4/part1/script.js
@@ -19,22 +19,21 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const characters = [" Willy the Goblin"," Big Daddy"," Father Christmas"];
+const places = [" the soup kitchen", " Disneyland", " the White House"];
+const events = [" spontaneously combusted"," melted into a puddle on the sidewalk"," turned into a slug and slithered away"];
 
-// the soup kitchen
-// Disneyland
-// the White House
 
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
 
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+    const randomCharacter = randomValueFromArray(characters); 
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
+    
+    const storyText = "It was 94 Fahrenheit outside, ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomcharacter} weighs 300 pounds, and it was a hot day."
+
 
   return storyText;
 }
